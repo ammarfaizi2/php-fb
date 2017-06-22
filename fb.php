811e40843ff1ff3929ac066689c0d39b89c378c7
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-22 20:24:50 +0700

    Fix namespace

--- fb.php
+++ fb.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
-use PHPFB\PHPFB;
+use PHPFB\PHPFBHandler;
 
 define("data", __DIR__."/data");
 define("fb_data", data."/fb_data");
@@ -9,4 +9,4 @@ is_dir(data) or mkdir(data);
 is_dir(fb_data) or mkdir(fb_data);
 (is_dir(data) and is_dir(fb_data)) or die("Gagal membuat directory !");
 
-PHPFB::run();
+PHPFBHandler::run();
